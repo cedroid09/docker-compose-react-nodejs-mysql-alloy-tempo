0ed910a8ebfba1acaa6e5d5d6632e368bb998ef6
Cedric Poottaren <cedric.poottaren@orange.com>
2024-10-28 17:43:23 +0400

    fix: otel config

--- bezkoder-api/app/config/otel-config.js
+++ bezkoder-api/app/config/otel-config.js
@@ -1,9 +1,10 @@
+// otel-config.js
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-grpc');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-http'); // Use the HTTP exporter
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
 const traceExporter = new OTLPTraceExporter({
-  url: 'http://10.4.0.2:4319', // Alloy connector endpoint
+  url: 'http://10.4.0.2:4318/v1/traces', // Adjust to the HTTP endpoint
 });
 
 const sdk = new NodeSDK({
