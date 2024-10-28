524d1a55d26903c29df414c3cafed1b87eca825b
Cedric Poottaren <cedric.poottaren@orange.com>
2024-10-28 18:15:14 +0400

    fix: otel config

--- bezkoder-api/app/config/otel-config.js
+++ bezkoder-api/app/config/otel-config.js
@@ -1,14 +1,20 @@
 // otel-config.js
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-http'); // Use the HTTP exporter
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-http');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { Resource } = require('@opentelemetry/resources');
+const { SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 
+// Set the service name here
 const traceExporter = new OTLPTraceExporter({
-  url: 'http://10.4.0.2:4320/v1/traces', // Adjust to the HTTP endpoint
+  url: 'http://10.4.0.2:4320/v1/traces',
 });
 
 const sdk = new NodeSDK({
   traceExporter,
+  resource: new Resource({
+    [SERVICE_NAME]: 'demo-node-app', // Customize the service name
+  }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
